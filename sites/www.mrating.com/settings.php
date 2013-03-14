f5f0233376ebe5363100fa24360b9c19eef99468
Martin Ma <liangtwin@gmail.com>
2013-03-14 09:31:52 +0800

    mrating-7.x-1.0-dev

--- sites/www.mrating.com/settings.php
+++ sites/www.mrating.com/settings.php
@@ -256,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '_om7LqWv9jLOV4fgRL42Cqu0BsocqaWXR2v3AgomNKg';
+$drupal_hash_salt = 'hiDSKNUY5mUKZncIJhD0fQapiMuBpgauRXCUDc98Xtk';
 
 /**
  * Base URL (optional).
