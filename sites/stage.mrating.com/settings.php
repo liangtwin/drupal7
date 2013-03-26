6c4b7293ce32544cca7492014fa0f081ca5faa1f
Martin Ma <liangtwin@gmail.com>
2013-03-26 17:49:48 +0800

    modify settings.php and default.settings.httpauth.php

--- sites/stage.mrating.com/settings.php
+++ sites/stage.mrating.com/settings.php
@@ -552,54 +552,36 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  */
 # $conf['allow_authorize_operations'] = FALSE;
 
-$realm = 'Restricted area';
+// Make sure drush keeps working. 
+// Modified from function drush_verify_cli()
+$cli = (php_sapi_name() == 'cli');
 
-//user => password
-$users = array('admin' => 'mypass', 'guest' => 'guest');
-
-
-if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.
-           '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
-
-    die('Text to send if user hits Cancel button');
-}
-
-
-// analyze the PHP_AUTH_DIGEST variable
-if (!($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) ||
-    !isset($users[$data['username']]))
-    die('Wrong Credentials!');
-
-
-// generate the valid response
-$A1 = md5($data['username'] . ':' . $realm . ':' . $users[$data['username']]);
-$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
-$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
-
-if ($data['response'] != $valid_response)
-    die('Wrong Credentials!');
-
-// ok, valid username & password
-//echo 'You are logged in as: ' . $data['username'];
-
-
-// function to parse the http auth header
-function http_digest_parse($txt)
-{
-    // protect against missing data
-    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
-    $data = array();
-    $keys = implode('|', array_keys($needed_parts));
-
-    preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
-
-    foreach ($matches as $m) {
-        $data[$m[1]] = $m[3] ? $m[3] : $m[4];
-        unset($needed_parts[$m[1]]);
+// PASSWORD PROTECT NON-PRODUCTION SITES (i.e. staging/dev)
+if (!$cli) {
+  if (!isset($_GET['cron_key'])) {
+    $username = 'mrating';
+    $password = 'Mrating2323';
+    $redirect_required = FALSE;
+    
+    // PHP-cgi fix
+   /* $a = base64_decode( substr($_SERVER["REMOTE_USER"],6)) ;
+	print_r($_SERVER["REMOTE_USER"]);
+    if ( (strlen($a) == 0) || ( strcasecmp($a, ":" ) == 0 )) {
+      header( 'WWW-Authenticate: Basic realm="Private"' );
+      header( 'HTTP/1.0 401 Unauthorized' );
     }
-
-    return $needed_parts ? false : $data;
+    else {
+      list($name, $pass) = explode(':', $a);
+      $_SERVER['PHP_AUTH_USER'] = $name;
+      $_SERVER['PHP_AUTH_PW'] = $pass;
+    }*/
+	
+    if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
+      header('WWW-Authenticate: Basic realm="This site is protected"');
+      header('HTTP/1.0 401 Unauthorized');
+      // Fallback message when the user presses cancel / escape
+      echo 'Access denied';
+      exit;
+    }
+  }
 }
-
