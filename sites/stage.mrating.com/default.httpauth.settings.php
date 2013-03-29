347c34a863a134efa8d355982925a3d32246e095
Martin Ma <liangtwin@gmail.com>
2013-03-30 00:10:30 +0800

    add http authentication to stage.mrating.com

--- sites/stage.mrating.com/default.httpauth.settings.php
+++ sites/stage.mrating.com/default.httpauth.settings.php
@@ -564,8 +564,8 @@ if (!$cli) {
     $redirect_required = FALSE;
     
     // PHP-cgi fix
-   /* $a = base64_decode( substr($_SERVER["REMOTE_USER"],6)) ;
-	print_r($_SERVER["REMOTE_USER"]);
+    $a = base64_decode( substr($_SERVER["REMOTE_USER"],6)) ;
+
     if ( (strlen($a) == 0) || ( strcasecmp($a, ":" ) == 0 )) {
       header( 'WWW-Authenticate: Basic realm="Private"' );
       header( 'HTTP/1.0 401 Unauthorized' );
@@ -574,7 +574,7 @@ if (!$cli) {
       list($name, $pass) = explode(':', $a);
       $_SERVER['PHP_AUTH_USER'] = $name;
       $_SERVER['PHP_AUTH_PW'] = $pass;
-    }*/
+    }
 	
     if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
       header('WWW-Authenticate: Basic realm="This site is protected"');
