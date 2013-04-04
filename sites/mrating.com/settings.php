e007945d105b5bfa192836b01ed617e2a1834407
Martin Ma <liangtwin@gmail.com>
2013-04-04 14:10:51 +0800

    fix taxonomy display issue

--- sites/mrating.com/settings.php
+++ sites/mrating.com/settings.php
@@ -256,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = 'MQJewvGvn8YpxhtEUXN361pQnX15r0tBtYlaD41tCjY';
+$drupal_hash_salt = 'xxkOlIzXBD0hixd_e-tSgvSvIexCmVAm16JWwmGpqZE';
 
 /**
  * Base URL (optional).
